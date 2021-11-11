c81cabc53f957100a73415ee794716ba53781b04
Jacob <a>
2021-11-11 14:30:30 +0000

    Add dependancy for React Router

--- src/comp6000/assets/src/react-shim.js
+++ src/comp6000/assets/src/react-shim.js
@@ -1,3 +1,10 @@
 // This is inserted into all JS files by esbuild
 import * as React from 'react'
-export { React }
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import ReactDOM from 'react-dom';
+export { React, BrowserRouter, Switch, Route, Link, ReactDOM }

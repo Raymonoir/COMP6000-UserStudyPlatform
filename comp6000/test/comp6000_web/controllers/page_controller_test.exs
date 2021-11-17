99f2a7a7b8c493d5d02b266d3ee2c8d5e295e7c9
Raymond Ward <rayward1337@gmail.com>
2021-11-17 22:57:55 +0000

    Removed broken and uneeded test

--- comp6000/test/comp6000_web/controllers/page_controller_test.exs
+++ comp6000/test/comp6000_web/controllers/page_controller_test.exs
@@ -1,8 +1,3 @@
 defmodule Comp6000Web.PageControllerTest do
   use Comp6000Web.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
 end

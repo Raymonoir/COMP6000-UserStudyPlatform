efe3fcd293d6660d8ef2ba8706371603f8adf9e8
Raymond Ward <rayward1337@gmail.com>
2021-11-22 23:02:38 +0000

    PageController error render function

--- comp6000/lib/comp6000_web/controllers/page_controller.ex
+++ comp6000/lib/comp6000_web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule Comp6000Web.PageController do
   use Comp6000Web, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def error(conn, _params) do
+    json(conn, %{error: "404"})
   end
 end

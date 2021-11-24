9abcf1bba374882664a8114d1629bde3aa1794d5
Raymond Ward <rayward1337@gmail.com>
2021-11-24 18:05:40 +0000

    added check to ensure user is not nil

--- comp6000/lib/comp6000_web/authentication.ex
+++ comp6000/lib/comp6000_web/authentication.ex
@@ -4,6 +4,14 @@ defmodule Comp6000Web.Authentication do
   def login(username, password) do
     user = Users.get_user_by(username: username)
 
+    if user != nil do
+      check_pass(user, password)
+    else
+      false
+    end
+  end
+
+  defp check_pass(user, password) do
     case Bcrypt.check_pass(user, password) do
       {:ok, _user} ->
         true

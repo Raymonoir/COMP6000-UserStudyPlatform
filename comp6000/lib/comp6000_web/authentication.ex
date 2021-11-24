382d7ec4643eb76ad5504d3eb9e4d0dd860eb484
Raymond Ward <rayward1337@gmail.com>
2021-11-24 12:59:20 +0000

    slightly changed function

--- comp6000/lib/comp6000_web/authentication.ex
+++ comp6000/lib/comp6000_web/authentication.ex
@@ -2,12 +2,14 @@ defmodule Comp6000Web.Authentication do
   alias Comp6000.Contexts.Users
 
   def login(username, password) do
-    # Get user password from database
     user = Users.get_user_by(username: username)
 
     case Bcrypt.check_pass(user, password) do
-      {:error, _reason} -> false
-      _ -> true
+      {:ok, _user} ->
+        true
+
+      _ ->
+        false
     end
   end
 end

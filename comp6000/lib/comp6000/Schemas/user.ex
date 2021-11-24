8da33350b9e43bf21bf1743d438f867957b6793e
Raymond Ward <rayward1337@gmail.com>
2021-11-24 12:58:26 +0000

    added more constraints to fields, added method to set password hash and also an update changeset to update user, work in progress

--- comp6000/lib/comp6000/Schemas/user.ex
+++ comp6000/lib/comp6000/Schemas/user.ex
@@ -16,8 +16,35 @@ defmodule Comp6000.Schemas.User do
 
   def changeset(%User{} = user, params) do
     user
-    |> cast(params, [:username, :firstname, :lastname, :email, :password_hash])
-    |> validate_required([:username, :email])
+    |> cast(params, [:username, :firstname, :lastname, :email, :password])
+    |> validate_required([:username, :email, :password])
+    |> validate_changeset()
+    |> set_password_hash()
+  end
+
+  def update_changeset(%User{} = user, params) do
+    user
+    |> cast(params, [:username, :firstname, :lastname, :email, :password])
+    |> validate_changeset()
+  end
+
+  defp validate_changeset(changeset) do
+    changeset
+    |> validate_length(:username, min: 1, max: 100)
     |> unique_constraint(:username)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+    |> validate_length(:password, min: 10, max: 100)
+  end
+
+  defp set_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+        put_change(changeset, :password, nil)
+
+      _else ->
+        changeset
+    end
   end
 end

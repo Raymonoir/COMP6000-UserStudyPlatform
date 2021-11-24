d41841aed91f959df24f4d79c782a094184a4802
Raymond Ward <rayward1337@gmail.com>
2021-11-24 12:59:53 +0000

    altered user controller to be a bit cleaner

--- comp6000/lib/comp6000_web/controllers/users_controller.ex
+++ comp6000/lib/comp6000_web/controllers/users_controller.ex
@@ -1,12 +1,11 @@
 defmodule Comp6000Web.UsersController do
   use Comp6000Web, :controller
 
-  def login(conn, %{"password" => password, "username" => username}) do
-    res = Comp6000Web.Authentication.login(username, password)
-    json(conn, %{login: res})
+  def login(conn, %{"password" => password, "username" => username} = _params) do
+    json(conn, %{login: Comp6000Web.Authentication.login(username, password)})
   end
 
-  def login(conn, params) do
+  def login(conn, _params) do
     json(conn, %{login: nil})
   end
 end

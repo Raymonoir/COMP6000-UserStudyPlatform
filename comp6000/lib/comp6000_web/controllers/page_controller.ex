998d902996391c179894a62b0da9f38b586f733b
Jacob <a>
2021-11-24 12:30:32 +0000

    Restore route needed to load react. All routing after that is handled by react

--- comp6000/lib/comp6000_web/controllers/page_controller.ex
+++ comp6000/lib/comp6000_web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule Comp6000Web.PageController do
   use Comp6000Web, :controller
 
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
   def error(conn, _params) do
     json(conn, %{error: "404"})
   end

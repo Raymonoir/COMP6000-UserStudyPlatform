641728cb9b8ab91c38ab1abfcce60a2d6833eee1
Raymond Ward <rayward1337@gmail.com>
2021-12-15 11:53:22 +0000

    removed helper function to new file

--- comp6000/lib/comp6000_web/controllers/users_controller.ex
+++ comp6000/lib/comp6000_web/controllers/users_controller.ex
@@ -1,6 +1,5 @@
 defmodule Comp6000Web.UsersController do
   use Comp6000Web, :controller
-  import Ecto.Changeset
   import Plug.Conn
 
   def login(conn, %{"username" => username, "password" => password} = _params) do
@@ -20,7 +19,7 @@ defmodule Comp6000Web.UsersController do
   def create(conn, params) do
     case Comp6000.Contexts.Users.create_user(params) do
       {:ok, user} -> json(conn, %{created: user.username})
-      {:error, changeset} -> json(conn, %{error: get_changeset_errors(changeset)})
+      {:error, changeset} -> json(conn, %{error: Helpers.get_changeset_errors(changeset)})
     end
   end
 
@@ -37,19 +36,5 @@ defmodule Comp6000Web.UsersController do
     conn
     |> put_session(:username, user.username)
     |> configure_session(renew: true)
-  end
-
-  defp get_changeset_errors(changeset) do
-    errors_map =
-      traverse_errors(changeset, fn {msg, opts} ->
-        Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
-          opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
-        end)
-      end)
-
-    errors_map
-    |> Map.keys()
-    |> Enum.map(fn key -> "#{key} #{errors_map[key]}" end)
-    |> Enum.join(",")
   end
 end

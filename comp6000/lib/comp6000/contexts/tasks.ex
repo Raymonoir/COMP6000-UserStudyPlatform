69f2dedb85be84575db1018d20b64f79eeab7f53
Raymond Ward <rayward1337@gmail.com>
2021-12-15 12:04:18 +0000

    created contexts for inserting data into db

---
+++ comp6000/lib/comp6000/contexts/tasks.ex
@@ -0,0 +1,23 @@
+defmodule Comp6000.Contexts.Tasks do
+  alias Comp6000.Repo
+  alias Comp6000.Schemas.Task
+
+  def get_task_by(params) do
+    Repo.get_by(Task, params)
+  end
+
+  def create_task(params \\ %{}) do
+    %Task{}
+    |> Task.changeset(params)
+    |> Repo.insert()
+  end
+
+  def update_task(%Task{} = task, params) do
+    Task.update_changeset(task, params)
+    |> Repo.update()
+  end
+
+  def delete_task(%Task{} = task) do
+    Repo.delete(task)
+  end
+end

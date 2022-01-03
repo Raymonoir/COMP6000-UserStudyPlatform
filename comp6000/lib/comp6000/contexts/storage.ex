0011054ba387d97bcdfce1abb62bcea6f23d3f8c
Raymond Ward <raymondsw99@gmail.com>
2022-01-03 14:41:52 +0000

    add config values for storage files

--- comp6000/lib/comp6000/contexts/storage.ex
+++ comp6000/lib/comp6000/contexts/storage.ex
@@ -1,7 +1,10 @@
 defmodule Comp6000.Contexts.Storage do
-  alias Comp6000.Schemas.Study
+  alias Comp6000.Schemas.{Study, Task, Result}
 
   @storage_path Application.get_env(:comp6000, :storage_directory_path)
+  @file_extension Application.get_env(:comp6000, :storage_file_extension)
+  @completed_extension Application.get_env(:comp6000, :completed_file_extension)
+  @chunk_delimiter Application.get_env(:comp6000, :chunk_delimiter)
 
   def create_study_directory(%Study{} = study) do
     study_id = study.id
@@ -31,7 +34,7 @@ defmodule Comp6000.Contexts.Storage do
     end
   end
 
-  def create_task_directory(task) do
+  def create_task_directory(%Task{} = task) do
     path = "#{@storage_path}/#{task.study_id}"
 
     if File.exists?(path) do
@@ -47,7 +50,7 @@ defmodule Comp6000.Contexts.Storage do
     end
   end
 
-  def delete_task_directory(task) do
+  def delete_task_directory(%Task{} = task) do
     path = "#{@storage_path}/#{task.study_id}/#{task.id}"
 
     if File.exists?(path) do
@@ -58,6 +61,78 @@ defmodule Comp6000.Contexts.Storage do
         {:error, _reason} ->
           :error
       end
+    else
+      :error
+    end
+  end
+
+  def create_result_file(%Result{} = result) do
+    task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
+    study_id = task.study_id
+    task_id = task.id
+
+    path = "#{@storage_path}/#{study_id}/#{task_id}"
+
+    if File.exists?(path) do
+      case File.write("#{path}/#{result.unique_participant_id}.#{@file_extension}", "") do
+        :ok ->
+          :ok
+
+        {:error, _reason} ->
+          :error
+      end
+    else
+      :error
+    end
+  end
+
+  def append_result_file(%Result{} = result, chunk) do
+    task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
+    study_id = task.study_id
+    task_id = task.id
+
+    path =
+      "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}.#{@file_extension}"
+
+    if File.exists?(path) do
+      {:ok, current_content} = File.read(path)
+      new_content = "#{current_content}#{@chunk_delimiter}#{chunk}"
+      File.write(path, new_content)
+    else
+      :error
+    end
+  end
+
+  def complete_file_storage(%Result{} = result) do
+    task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
+    study_id = task.study_id
+    task_id = task.id
+
+    path_no_ext = "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}"
+
+    if File.exists?("#{path_no_ext}.#{@file_extension}") do
+      {:ok, content} = File.read("#{path_no_ext}.#{@file_extension}")
+      gzipped_content = :zlib.gzip(content)
+      File.write("#{path_no_ext}.#{@file_extension}", gzipped_content)
+
+      :ok =
+        File.rename("#{path_no_ext}.#{@file_extension}", "#{path_no_ext}.#{@completed_extension}")
+    else
+      :error
+    end
+  end
+
+  def get_completed_file_content(%Result{} = result) do
+    task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
+    study_id = task.study_id
+    task_id = task.id
+
+    path =
+      "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}.#{@completed_extension}"
+
+    if File.exists?(path) do
+      {:ok, content} = File.read(path)
+      :zlib.gunzip(content)
     else
       :error
     end
--- comp6000/test/comp6000/contexts/storage_test.exs
+++ comp6000/test/comp6000/contexts/storage_test.exs
@@ -1,8 +1,11 @@
 defmodule Comp6000.Contexts.StorageTest do
   use Comp6000.DataCase, async: true
-  alias Comp6000.Contexts.{Storage, Studies, Users, Tasks}
+  alias Comp6000.Contexts.{Storage, Studies, Users, Tasks, Results}
 
   @storage_path Application.get_env(:comp6000, :storage_directory_path)
+  @file_extension Application.get_env(:comp6000, :storage_file_extension)
+  @completed_extension Application.get_env(:comp6000, :completed_file_extension)
+
   setup do
     {:ok, user} =
       Users.create_user(%{username: "Ray123", email: "Ray@email.com", password: "password12345"})
@@ -13,9 +16,16 @@ defmodule Comp6000.Contexts.StorageTest do
     {:ok, task} =
       Tasks.create_task(%{content: "What is 2*2?", task_number: 1, study_id: study.id})
 
+    {:ok, result} =
+      Results.create_result(%{
+        task_id: task.id,
+        unique_participant_id: "567f56d67s67as76d7s8",
+        content: "3"
+      })
+
     on_exit(&clear_local_storage/0)
 
-    %{study: study, task: task}
+    %{study: study, task: task, result: result}
   end
 
   # An exceedingly nasty function to delete all files and directories within local-storage once tests are complete
@@ -73,6 +83,103 @@ defmodule Comp6000.Contexts.StorageTest do
       :ok = Storage.delete_task_directory(task)
 
       refute File.exists?("#{@storage_path}/#{study.id}/#{task.id}")
+    end
+  end
+
+  describe "create_result_file/1" do
+    test "creates file at correct location", %{study: study, task: task, result: result} do
+      :ok = File.mkdir("#{@storage_path}/#{study.id}")
+      :ok = File.mkdir("#{@storage_path}/#{study.id}/#{task.id}")
+
+      :ok = Storage.create_result_file(result)
+
+      assert File.exists?(
+               "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@file_extension}"
+             )
+    end
+  end
+
+  describe "append_result_file/2" do
+    test "appends data to created results file", %{
+      study: study,
+      task: task,
+      result: result
+    } do
+      :ok = File.mkdir("#{@storage_path}/#{study.id}")
+      :ok = File.mkdir("#{@storage_path}/#{study.id}/#{task.id}")
+
+      path =
+        "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@file_extension}"
+
+      :ok = File.write(path, "")
+
+      chunk1 = "chunk1data"
+      chunk2 = "chunk2data"
+      chunk3 = "chunk3data"
+
+      :ok = Storage.append_result_file(result, chunk1)
+      {:ok, ",chunk1data"} = File.read(path)
+
+      :ok = Storage.append_result_file(result, chunk2)
+      {:ok, ",chunk1data,chunk2data"} = File.read(path)
+
+      :ok = Storage.append_result_file(result, chunk3)
+      {:ok, ",chunk1data,chunk2data,chunk3data"} = File.read(path)
+    end
+  end
+
+  describe "complete_file_storage/1" do
+    test "gzips content and renames file with .gzip file extension", %{
+      study: study,
+      task: task,
+      result: result
+    } do
+      :ok = File.mkdir("#{@storage_path}/#{study.id}")
+      :ok = File.mkdir("#{@storage_path}/#{study.id}/#{task.id}")
+
+      path =
+        "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@file_extension}"
+
+      unzipped_content = "A few chunks"
+      :ok = File.write(path, unzipped_content)
+
+      :ok = Storage.complete_file_storage(result)
+
+      assert File.exists?(
+               "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@completed_extension}"
+             )
+
+      refute File.exists?(
+               "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@file_extension}"
+             )
+
+      {:ok, gzipped_content} =
+        File.read(
+          "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@completed_extension}"
+        )
+
+      result = :zlib.gunzip(gzipped_content)
+      assert result == unzipped_content
+    end
+  end
+
+  describe "get_completed_file_content/1" do
+    test "returns content stored in a completed file", %{
+      study: study,
+      task: task,
+      result: result
+    } do
+      :ok = File.mkdir("#{@storage_path}/#{study.id}")
+      :ok = File.mkdir("#{@storage_path}/#{study.id}/#{task.id}")
+
+      path =
+        "#{@storage_path}/#{study.id}/#{task.id}/#{result.unique_participant_id}.#{@completed_extension}"
+
+      unzipped_content = "A few chunks"
+      gzipped_content = :zlib.gzip(unzipped_content)
+      :ok = File.write(path, gzipped_content)
+
+      assert unzipped_content == Storage.get_completed_file_content(result)
     end
   end
 end

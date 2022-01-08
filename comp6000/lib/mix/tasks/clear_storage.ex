2c7547bb19c01e88b907cbcc03937c1e35eb908c
Raymond Ward <raymondsw99@gmail.com>
2022-01-08 22:17:33 +0000

    changed mix task

--- comp6000/lib/mix/tasks/clear_storage.ex
+++ comp6000/lib/mix/tasks/clear_storage.ex
@@ -2,7 +2,9 @@ defmodule Mix.Tasks.ClearStorage do
   use Mix.Task
 
   def run(args) do
-    if hd(args) == "study" do
+    first = List.first(args)
+
+    if first == "study" do
       Enum.map(tl(args), fn study_id -> Mix.shell().cmd("rm -r local-storage/#{study_id}") end)
     else
       Mix.shell().cmd("rm -r local-storage/*")

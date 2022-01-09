b40d7f378f261b689778c94fe9153b498bd389dd
Crump1994 <47825319+Crump1994@users.noreply.github.com>
2022-01-09 18:12:17 +0000

    Use a child process to contain a user's code
    This means that we can have a timeout to stop a users code from running for a long time by killing the child process containing it

---
+++ comp6000/codeserver/container.js
@@ -0,0 +1,54 @@
+const { NodeVM } = require('vm2');
+
+const vm = new NodeVM({
+    console: 'redirect',
+});
+
+vm.on('console.log', data => {
+    console.log('vm log data: ', data);
+    logs.push({
+        type: 'log',
+        data: data
+    });
+});
+
+vm.on('console.warn', data => {
+    console.log('vm warn data: ', data);
+    logs.push({
+        type: 'warn',
+        data: data
+    });
+});
+
+vm.on('console.error', data => {
+    console.log('vm error data: ', data);
+    logs.push({
+        type: 'error',
+        data: data
+    });
+});
+
+let logs = [];
+let output;
+let error;
+
+process.on('message', msg => {
+    try {
+        userCode = msg.code + '\nmodule.exports = ' + msg.run;
+        const container = vm.run(userCode);
+        output = container('5');
+    } catch (e) {
+        console.error(e.stack);
+        error = e.message;
+    }
+
+    let result = {
+        logs: logs,
+        output: output
+    };
+    if (error) {
+        result.error = error;
+    }
+    process.send(result);
+})
+

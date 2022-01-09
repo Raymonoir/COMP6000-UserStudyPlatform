27891b7c865598131a2ccad72e171fcbabe62cc0
jacmol7 <jacob.amazing1@gmail.com>
2022-01-09 15:10:31 +0000

    Begin creating a component to execute and display the results of running user written code

---
+++ comp6000/assets/src/components/CodeRunner.js
@@ -0,0 +1,66 @@
+class CodeRunner extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ran: false
+        }
+    }
+
+    componentDidMount() {
+        const abort = new AbortController();
+
+        const request = fetch('http://localhost:3000/run', {
+            signal: abort.signal,
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ code: this.props.code, run: this.props.run })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    ran: true,
+                    result: data
+                });
+            });
+
+        this.setState({
+            request: request,
+            abortController: abort
+        });
+    }
+
+    componentWillUnmount() {
+        this.state.abortController.abort();
+    }
+
+    render() {
+        if (!this.state.ran) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <div>
+                <div>
+                    {
+                        this.state.result.logs.map((line, i) => {
+                            return <p key={i}>{line.data}</p>
+                        })
+                    }
+                </div>
+                <div>
+                    {!this.state.result.error &&
+                        <p>output: {this.state.result.output}</p>}
+
+                    {this.state.result.error &&
+                        <p>error: {this.state.result.error}</p>}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CodeRunner;

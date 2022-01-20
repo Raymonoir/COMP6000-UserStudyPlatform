f436fab34bd9b86fe28e44379f811892e3ccf75d
Raymond Ward <raymondsw99@gmail.com>
2022-01-20 22:34:36 +0000

    changed creating functions so files are also created when functions are called

--- comp6000/lib/comp6000/contexts/results.ex
+++ comp6000/lib/comp6000/contexts/results.ex
@@ -1,18 +1,37 @@
 defmodule Comp6000.Contexts.Results do
+  import Ecto.Query
   alias Comp6000.Repo
-  alias Comp6000.Schemas.Result
+  alias Comp6000.Schemas.{Result, Task}
+  alias Comp6000.Contexts.Storage
 
   def get_result_by(params) do
     Repo.get_by(Result, params)
   end
 
   def create_result(params \\ %{}) do
-    %Result{}
-    |> Result.changeset(params)
-    |> Repo.insert()
+    case %Result{}
+         |> Result.changeset(params)
+         |> Repo.insert() do
+      {:ok, result} ->
+        Storage.create_result_file(result)
+        {:ok, result}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
   end
 
   def delete_result(%Result{} = result) do
     Repo.delete(result)
+  end
+
+  def get_results_for_task(%Task{} = task) do
+    Repo.preload(task, :results)
+  end
+
+  def get_result_for_task_uuid(task, uuid) do
+    query = from(r in Result, where: r.task_id == ^task.id and r.unique_participant_id == ^uuid)
+
+    Repo.all(query)
   end
 end
--- comp6000/lib/comp6000/contexts/studies.ex
+++ comp6000/lib/comp6000/contexts/studies.ex
@@ -2,6 +2,7 @@ defmodule Comp6000.Contexts.Studies do
   import Ecto.Query
   alias Comp6000.Repo
   alias Comp6000.Schemas.{Study, User}
+  alias Comp6000.Contexts.Storage
 
   def get_all_studies() do
     Repo.all(Study)
@@ -12,9 +13,16 @@ defmodule Comp6000.Contexts.Studies do
   end
 
   def create_study(params \\ %{}) do
-    %Study{}
-    |> Study.changeset(params)
-    |> Repo.insert()
+    case %Study{}
+         |> Study.changeset(params)
+         |> Repo.insert() do
+      {:ok, study} ->
+        Storage.create_study_directory(study)
+        {:ok, study}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
   end
 
   def update_study(%Study{} = study, params) do
@@ -31,5 +39,9 @@ defmodule Comp6000.Contexts.Studies do
     query = from(s in Study, where: s.username == ^user.username)
 
     Repo.all(query)
+  end
+
+  def get_all_for_study(%Study{} = study) do
+    study = Repo.preload(study, tasks: [:results, :answer], user: [])
   end
 end

78c7c792dcbe8d7a8c2ce983fd17d6bd76252b03
Raymond Ward <raymondsw99@gmail.com>
2022-01-31 14:00:45 +0000

    created functions to now create a directory at the unique_participant_id level and create two files, one for compile data and the other for replay data

--- comp6000/lib/comp6000/contexts/storage.ex
+++ comp6000/lib/comp6000/contexts/storage.ex
@@ -8,6 +8,9 @@ defmodule Comp6000.Contexts.Storage do
   @storage_file_start Application.get_env(:comp6000, :storage_file_start)
   @storage_file_end Application.get_env(:comp6000, :storage_file_end)
 
+  @compile_data_filename "compile-data"
+  @replay_data_filename "replay-data"
+
   def create_study_directory(%Study{} = study) do
     study_id = study.id
 
@@ -68,7 +71,7 @@ defmodule Comp6000.Contexts.Storage do
     end
   end
 
-  def create_result_file(%Result{} = result) do
+  def create_participant_directory(%Result{} = result) do
     task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
     study_id = task.study_id
     task_id = task.id
@@ -76,28 +79,47 @@ defmodule Comp6000.Contexts.Storage do
     path = "#{@storage_path}/#{study_id}/#{task_id}"
 
     if File.exists?(path) do
-      case File.write(
-             "#{path}/#{result.unique_participant_id}.#{@file_extension}",
-             @storage_file_start
-           ) do
+      case File.mkdir("#{path}/#{result.unique_participant_id}") do
         :ok ->
           result
 
-        {:error, _reason} ->
-          :error
+        {:error, :eexist} ->
+          result
+
+        {:error, reason} ->
+          {:error, reason}
       end
     else
       :error
     end
   end
 
-  def append_result_file(%Result{} = result, chunk) do
+  def create_participant_files(%Result{} = result) do
     task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
     study_id = task.study_id
     task_id = task.id
 
-    path =
-      "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}.#{@file_extension}"
+    :ok =
+      File.write(
+        "#{get_participant_files_path(result, :replay)}.#{@file_extension}",
+        @storage_file_start
+      )
+
+    :ok =
+      File.write(
+        "#{get_participant_files_path(result, :compile)}.#{@file_extension}",
+        @storage_file_start
+      )
+
+    result
+  end
+
+  def append_data(%Result{} = result, chunk, filetype) do
+    task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
+    study_id = task.study_id
+    task_id = task.id
+
+    path = "#{get_participant_files_path(result, filetype)}.#{@file_extension}"
 
     if File.exists?(path) do
       {:ok, current_content} = File.read(path)
@@ -113,12 +135,12 @@ defmodule Comp6000.Contexts.Storage do
     end
   end
 
-  def complete_file_storage(%Result{} = result) do
+  def complete_data(%Result{} = result, filetype) do
     task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
     study_id = task.study_id
     task_id = task.id
 
-    path_no_ext = "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}"
+    path_no_ext = "#{get_participant_files_path(result, filetype)}"
 
     if File.exists?("#{path_no_ext}.#{@file_extension}") do
       {:ok, content} = File.read("#{path_no_ext}.#{@file_extension}")
@@ -132,19 +154,32 @@ defmodule Comp6000.Contexts.Storage do
     end
   end
 
-  def get_completed_file_content(%Result{} = result) do
+  def get_completed_data(%Result{} = result, filetype) do
     task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
     study_id = task.study_id
     task_id = task.id
 
-    path =
-      "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}.#{@completed_extension}"
+    path = "#{get_participant_files_path(result, filetype)}.#{@completed_extension}"
 
     if File.exists?(path) do
       {:ok, content} = File.read(path)
       :zlib.gunzip(content)
     else
       :error
+    end
+  end
+
+  defp get_participant_files_path(%Result{} = result, filetype) do
+    task = Comp6000.Contexts.Tasks.get_task_by(id: result.task_id)
+    study_id = task.study_id
+    task_id = task.id
+
+    case filetype do
+      :compile ->
+        "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}/#{@compile_data_filename}"
+
+      :replay ->
+        "#{@storage_path}/#{study_id}/#{task_id}/#{result.unique_participant_id}/#{@replay_data_filename}"
     end
   end
 end

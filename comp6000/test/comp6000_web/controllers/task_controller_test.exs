8dc835f3d3aaab28053c19353a5c7d9470adce19
Raymond Ward <raymondsw99@gmail.com>
2022-02-13 16:20:37 +0000

    a few more tests

--- comp6000/test/comp6000_web/controllers/task_controller_test.exs
+++ comp6000/test/comp6000_web/controllers/task_controller_test.exs
@@ -37,7 +37,7 @@ defmodule Comp6000Web.TaskControllerTest do
   }
 
   describe "POST /api/task/create" do
-    test "valid parameters creates task and directory", %{conn: conn, task1: task1} do
+    test "valid parameters creates single task and directory", %{conn: conn, task1: task1} do
       conn = post(conn, "/api/task/create", %{tasks: [task1]})
 
       result = json_response(conn, 200)
@@ -51,6 +51,48 @@ defmodule Comp6000Web.TaskControllerTest do
       assert stored_task.content == task1.content
 
       assert File.exists?("#{@storage_path}/#{task1.study_id}/#{stored_task.id}")
+    end
+
+    test "valid parameters creates multiple tasks", %{
+      conn: conn,
+      task2: task2,
+      task3: task3
+    } do
+      conn = post(conn, "/api/task/create", %{tasks: [task2, task3]})
+
+      result = json_response(conn, 200)
+
+      %{"created_tasks" => [id2, id3]} = result
+
+      stored_task2 = Tasks.get_task_by(id: id2)
+      stored_task3 = Tasks.get_task_by(id: id3)
+
+      assert stored_task2
+      assert stored_task3
+
+      assert stored_task2.content == task2.content
+      assert stored_task3.content == task3.content
+
+      # assert File.exists?("#{@storage_path}/#{task1.study_id}/#{stored_task.id}")
+    end
+
+    test "both valid and invalid parameters creates tasks where possible", %{
+      conn: conn,
+      task1: task1
+    } do
+      conn = post(conn, "/api/task/create", %{tasks: [task1, @invalid_task]})
+
+      result = json_response(conn, 200)
+
+      %{"created_tasks" => [id, "content can't be blank,study_id can't be blank"]} = result
+
+      stored_task = Tasks.get_task_by(id: id)
+
+      assert stored_task
+
+      assert stored_task.content == task1.content
+
+      # assert File.exists?("#{@storage_path}/#{task1.study_id}/#{stored_task.id}")
     end
 
     test "invalid parameters does not create task or directory", %{conn: conn} do

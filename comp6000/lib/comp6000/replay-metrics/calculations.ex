65b32ba087cec10ab5b84b7016b6a2367afa1d4a
Raymond Ward <raymondsw99@gmail.com>
2022-02-21 12:14:32 +0000

    added more metrics calculations

--- comp6000/lib/comp6000/replay-metrics/calculations.ex
+++ comp6000/lib/comp6000/replay-metrics/calculations.ex
@@ -21,13 +21,35 @@ defmodule Comp6000.ReplayMetrics.Calculations do
     elapsed = DateTime.diff(last_datetime, first_datetime, :second)
   end
 
-  def words_deleted(json_list) do
+  def character_count(json_list, action_type) do
+    events = content["events"]
+
+    Map.reduce(events, 0, fn [_time_added, keypress_data], acc ->
+      action = Map.get(keypress_data, "action")
+
+      if action == action_type do
+        keys =
+          Enum.reduce(Map.get(keypress_data, "lines"), fn data, acc ->
+            acc <> data
+          end)
+
+        String.length(keys)
+      else
+        acc
+      end
+    end)
   end
 
-  def idle_time do
-  end
+  def idle_time(json) do
+    events = content["events"]
 
-  def times_compiled do
+    Map.reduce(events, 0, fn [time_added, _keypress], acc ->
+      if time_added > 1000 do
+        acc + time_added - 1000
+      else
+        acc
+      end
+    end)
   end
 
   def get_lines_pasted(json_list) do
@@ -43,6 +65,16 @@ defmodule Comp6000.ReplayMetrics.Calculations do
           end
         end)
     end)
+  end
+
+  def proportion_lines_pasted do
+    get_line_count() / get_lines_pasted()
+  end
+
+  def get_inserted_charcters_proportion do
+    get_character_count("insertion") /
+      (get_character_count("insertion") +
+         get_character_count("deletion"))
   end
 
   def word_count(json_list) do
@@ -65,6 +97,51 @@ defmodule Comp6000.ReplayMetrics.Calculations do
           end
         end)
     end)
+  end
+
+  def most_common_error(json_list, error_type) do
+    error_map = %{}
+
+    Enum.each(content, fn data ->
+      error = data[error_type]
+
+      if Map.has_key?(error_map, error) do
+        Map.put(error_map, error, Map.get(error_map, error) + 1)
+      else
+        Map.put(error_map, error, 1)
+      end
+    end)
+
+    Enum.reduce(Map.keys(error_map), fn key, {acc_key, acc_val} ->
+      if Map.get(error_map, key) > acc_val do
+        {key, Map.get(error_map, key)}
+      else
+        {acc_key, acc_val}
+      end
+    end)
+  end
+
+  def time_idle_proportion() do
+    get_idle_time() / get_total_time(json_list)
+  end
+
+  def times_run() do
+    Enum.reduce(content, fn data, acc ->
+      acc + 1
+    end)
+  end
+
+  def proportion_passed() do
+    {pass_count, fail_count} =
+      Enum.reduce(content, fn data, {pass_count, fail_count} ->
+        if data["passed"] do
+          {pass_count + 1, fail_count}
+        else
+          {pass_count, fail_count + 1}
+        end
+      end)
+
+    pass_count / (pass_count + fail_count)
   end
 
   def words_per_minute(json_list) do

2467ee7d7aa2f9e08a1d20ea86581115ede4accc
Raymond Ward <raymondsw99@gmail.com>
2022-03-13 20:22:33 +0000

    made tests for all calculations and fixed some of the functions, changed example code

--- comp6000/lib/comp6000/replay-metrics/calculations.ex
+++ comp6000/lib/comp6000/replay-metrics/calculations.ex
@@ -6,67 +6,89 @@ defmodule Comp6000.ReplayMetrics.Calculations do
   @completed_extension Application.get_env(:comp6000, :completed_file_extension)
   @chunk_delimiter Application.get_env(:comp6000, :chunk_delimiter)
 
-  def calculate_all(Comp6000.Schemas.Study{} = study) do
-    participants = study.participant_list
+  def testing_data(datatype) do
+    case datatype do
+      :compile ->
+        Jason.decode!(File.read!("test/support/code-examples/loop-arr-error-compile.txt"))
 
-    Enum.reduce(participants, %{}. fn participant_uuid, acc ->
-
-    end)
+      :replay ->
+        Jason.decode!(File.read!("test/support/code-examples/multi-chunk-replay.txt"))
+    end
   end
 
-  def get_participant_data(participant_uuid, datatype) do
+  # def calculate_all(%Comp6000.Schemas.Study{} = study) do
+  #   participants = study.participant_list
 
+  #   Enum.reduce(participants, %{}, fn participant_uuid, acc ->
+  #     []
+  #   end)
+  # end
 
-    Jason.decode!(Storage.get_completed_file_content(result))
-  end
+  # def get_participant_data(participant_uuid, datatype) do
+  #   {:ok, metrics} = Metrics.get_metric_by(participant_uuid)
+  #   Jason.decode!(Storage.get_completed_data(metrics, datatype))
+  # end
 
-  def get_total_time(json_list) do
-    first = List.first(json_list)
-    last = List.last(json_list)
+  def get_total_time(data_map_list) do
+    first = List.first(data_map_list)
+    last = List.last(data_map_list)
 
     first_datetime = convert_timestamp(first["start"])
     last_datetime = convert_timestamp(last["end"])
     elapsed = DateTime.diff(last_datetime, first_datetime, :second)
   end
 
-  def character_count(json_list, action_type) do
-    events = content["events"]
+  # Action type = insert / remove
+  def get_character_count(data_map_list, action_type) do
+    Enum.reduce(data_map_list, 0, fn data_map, acc ->
+      events = data_map["events"]
 
-    Map.reduce(events, 0, fn [_time_added, keypress_data], acc ->
-      action = Map.get(keypress_data, "action")
-
-      if action == action_type do
-        keys =
-          Enum.reduce(Map.get(keypress_data, "lines"), fn data, acc ->
-            acc <> data
-          end)
-
-        String.length(keys)
-      else
-        acc
-      end
-    end)
-  end
-
-  def idle_time(json) do
-    events = content["events"]
-
-    Map.reduce(events, 0, fn [time_added, _keypress], acc ->
-      if time_added > 1000 do
-        acc + time_added - 1000
-      else
-        acc
-      end
-    end)
-  end
-
-  def get_lines_pasted(json_list) do
-    Enum.reduce(json_list, 0, fn chunk, acc ->
       acc +
-        Enum.reduce(chunk["events"], 0, fn event, chunk_acc ->
+        Enum.reduce(events, 0, fn [_time_added, keypress_data], acc ->
+          action = Map.get(keypress_data, "action")
+
+          if action == action_type do
+            keys =
+              Enum.reduce(Map.get(keypress_data, "lines"), fn data, acc ->
+                acc <> data
+              end)
+
+            acc + String.length(keys)
+          else
+            acc
+          end
+        end)
+    end)
+  end
+
+  def get_idle_time(data_map_list) do
+    Enum.reduce(data_map_list, 0, fn data_map, acc ->
+      events = data_map["events"]
+
+      acc +
+        Enum.reduce(events, 0, fn [time_added, _keypress], acc ->
+          if time_added > 1000 do
+            acc + time_added - 1000
+          else
+            acc
+          end
+        end)
+    end) / 1000
+  end
+
+  def get_words_pasted(data_map_list) do
+    Enum.reduce(data_map_list, 0, fn data_map, acc ->
+      acc +
+        Enum.reduce(data_map["events"], 0, fn event, chunk_acc ->
           lines = List.last(event)["lines"]
 
-          if lines != ["", ""] and length(lines) > 1 do
+          if lines not in [
+               ["", ""],
+               ["", "            ", "        }"],
+               ["", "        ", "    }"],
+               ["", "    ", "}"],
+               ["", "        ", "    "]
+             ] and length(lines) > 1 do
             chunk_acc + length(lines)
           else
             chunk_acc
@@ -75,52 +97,48 @@ defmodule Comp6000.ReplayMetrics.Calculations do
     end)
   end
 
-  def proportion_lines_pasted do
-    get_line_count() / get_lines_pasted()
-  end
+  # def proportion_lines_pasted do
+  #   get_line_count() / get_lines_pasted()
+  # end
 
-  def get_inserted_charcters_proportion do
-    get_character_count("insertion") /
-      (get_character_count("insertion") +
-         get_character_count("deletion"))
-  end
+  # def get_inserted_charcters_proportion do
+  #   get_character_count("insertion") /
+  #     (get_character_count("insertion") +
+  #        get_character_count("deletion"))
+  # end
 
-  def word_count(json_list) do
-    Enum.reduce(json_list, 0, fn chunk, acc ->
+  def get_word_count(data_map_list) do
+    Enum.reduce(data_map_list, 0, fn data_map, acc ->
       acc +
-        Enum.reduce(chunk["events"], 1, fn event, chunk_acc ->
+        Enum.reduce(data_map["events"], 0, fn event, chunk_acc ->
           lines = List.last(event)["lines"]
 
           if lines == [" "] do
             chunk_acc + 1
           else
             if length(lines) > 1 do
-              chunk_acc +
-                Enum.reduce(lines, 0, fn line, line_acc ->
-                  line_acc + length(String.split(line))
-                end)
+              chunk_acc + length(lines)
             else
               chunk_acc
             end
           end
         end)
-    end)
+    end) - 1
   end
 
-  def most_common_error(json_list, error_type) do
-    error_map = %{}
+  def get_most_common_error(data_map_list, error_type) do
+    error_map =
+      Enum.reduce(data_map_list, %{}, fn data, acc_map ->
+        error = data[error_type]
 
-    Enum.each(content, fn data ->
-      error = data[error_type]
+        if Map.has_key?(acc_map, error) do
+          Map.put(acc_map, error, Map.get(acc_map, error) + 1)
+        else
+          Map.put(acc_map, error, 1)
+        end
+      end)
 
-      if Map.has_key?(error_map, error) do
-        Map.put(error_map, error, Map.get(error_map, error) + 1)
-      else
-        Map.put(error_map, error, 1)
-      end
-    end)
-
-    Enum.reduce(Map.keys(error_map), fn key, {acc_key, acc_val} ->
+    Enum.reduce(Map.keys(error_map), {"", 0}, fn key, {acc_key, acc_val} ->
       if Map.get(error_map, key) > acc_val do
         {key, Map.get(error_map, key)}
       else
@@ -129,54 +147,54 @@ defmodule Comp6000.ReplayMetrics.Calculations do
     end)
   end
 
-  def time_idle_proportion() do
-    get_idle_time() / get_total_time(json_list)
-  end
+  # def time_idle_proportion() do
+  #   get_idle_time() / get_total_time(json_list)
+  # end
 
-  def times_run() do
-    Enum.reduce(content, fn data, acc ->
+  def get_times_run(data_map_list) do
+    Enum.reduce(data_map_list, 0, fn data, acc ->
       acc + 1
     end)
   end
 
-  def proportion_passed() do
-    {pass_count, fail_count} =
-      Enum.reduce(content, fn data, {pass_count, fail_count} ->
-        if data["passed"] do
-          {pass_count + 1, fail_count}
-        else
-          {pass_count, fail_count + 1}
-        end
-      end)
+  # def proportion_passed() do
+  #   {pass_count, fail_count} =
+  #     Enum.reduce(content, fn data, {pass_count, fail_count} ->
+  #       if data["passed"] do
+  #         {pass_count + 1, fail_count}
+  #       else
+  #         {pass_count, fail_count + 1}
+  #       end
+  #     end)
 
-    pass_count / (pass_count + fail_count)
+  #   pass_count / (pass_count + fail_count)
+  # end
+
+  def get_words_per_minute(data_map_list) do
+    time = get_total_time(data_map_list) / 60
+
+    word_count = get_word_count(data_map_list)
+
+    word_count / time
   end
 
-  def words_per_minute(json_list) do
-    time = get_total_time(json_list)
-
-    word_count = word_count(json_list)
-
-    round(word_count / time)
-  end
-
-  def get_line_count(json_list) do
-    Enum.reduce(json_list, 0, fn chunk, acc ->
-      acc +
-        Enum.reduce(chunk["events"], 0, fn event, chunk_acc ->
-          lines = List.last(event)["lines"]
-
-          if lines == ["", ""] do
-            chunk_acc + 1
+  def get_line_count(data_map_list) do
+    Enum.reduce(data_map_list, 0, fn data_map, acc ->
+      max =
+        Enum.reduce(data_map["events"], 0, fn [_time, map_event], chunk_acc ->
+          if chunk_acc < map_event["end"]["row"] do
+            map_event["end"]["row"]
           else
             chunk_acc
           end
         end)
-    end)
-  end
 
-  def json_testing() do
-    Jason.decode!(File.read!("lib/comp6000/replay-metrics/chunk.json"))
+      if acc < max do
+        max
+      else
+        acc
+      end
+    end)
   end
 
   def convert_timestamp(unix_time) do
